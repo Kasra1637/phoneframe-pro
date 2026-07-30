7f93c5cd56ae52c5063ae7ef13e337a3c4bc88bf
Kasra1637 <44356498+Kasra1637@users.noreply.github.com>
2026-07-30 14:37:19 -0700

    Integrate Cloudflare Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths(), cloudflare()],
   server: {
     host: true,
     port: 3000,
